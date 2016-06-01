b35af917231f468751be1ff75d7267171902df0a
Eva <Bresciano>
2016-06-01 12:17:25 -0600

    h

--- DeckOfOneCard/DeckOfOneCard/ImageController.swift
+++ DeckOfOneCard/DeckOfOneCard/ImageController.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import UIKit
+
+
+class ImageController {
+    
+    static func imageForURL(url: String, completion:((image: UIImage?) -> Void)) {
+        guard let url = NSURL(string: url) else {fatalError("Image URL optional, optional is nil")}
+        NetWorkController.performRequestForURL(url, httpMethod: .Get) { (data, error) in
+            guard let data = data else {
+                completion(image: nil)
+                return
+            }
+        }
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            <#code#>
+        })
+        completion(image: UIImage(data: data))
+    }
+}
