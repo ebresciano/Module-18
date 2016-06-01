b34248ef3483f83bbffaa14aada567d84938d53d
Eva <Bresciano>
2016-06-01 12:01:59 -0600

    h

--- DeckOfOneCard/DeckOfOneCard/CardController.swift
+++ DeckOfOneCard/DeckOfOneCard/CardController.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+class CardController {
+    
+    static let baseURL = NSURL(string: "http://deckofcardsapi.com/api/deck/new/draw/")
+    
+    static func drawCards(numberOfCards: Int, completion: ((card: [Card]) -> Void )) {
+        
+    }
+}
